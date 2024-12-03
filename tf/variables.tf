2333bcbd6c4f86f33c26c7a36c72c1f1e981ec00
Wynter Gailey <56845892+Nermeta@users.noreply.github.com>
2024-12-02 21:54:46 -0700

    edited tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,30 +1,44 @@
 variable "ami" {
   type = string
+  description = "Name of the machine"
+  default = "ami-0f19a1cf9c1469fd6"
 }
 
 variable "cluster_name" {
   type = string
+  description = "Name of the Cluster"
+  default = "ECS Instance - bb-cluster"
 }
 
 variable "container_image" {
-  type = string
+   description = "Docker image for the container"
+   default     = "itm350-qoi123-bb:1.0"
 }
 
 variable "container_port" {
   type = number
+   description = "Port that the container will listen on"
+   default     = 80
 }
 
 variable "instance_name_prefix" {
   type = string
+  description = "Prefix for instance names"
+  default     = "ecs-instance"
 }
 
 variable "instance_type" {
   type = string
+  description = "EC2 instance type for the ECS tasks"
+  default     = "t2.micro"
 }
 
 variable "lab_role" {
   type = string
+  description = "IAM role ARN for ECS instances"
+  default     = "arn:aws:iam::827224757504:role/LabRole" 
 }
+
 
 variable "region" {
   type        = string
